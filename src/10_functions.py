cb0207cec0ea4d5b46ff0cefdcb810b53424f28d
Daniel Gipson <dannygipson95@gmail.com>
2020-09-02 23:27:53 -0400

    completed first 11 items

--- src/10_functions.py
+++ src/10_functions.py
@@ -5,8 +5,19 @@
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
+def isEven(number):
+    print(number%2 == 0)
+
+isEven(num)
+        
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+def evenOrOdd(number):
+    if number % 2 == 0:
+        print("Even!")
+    else:
+        print("Odd")
 
+evenOrOdd(num)
